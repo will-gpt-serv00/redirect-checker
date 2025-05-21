36631a5f98d9785f1e65721a9d2d8db292482e98
will-gpt-serv00 <admin@will-gpt.ggff.net>
2025-05-22 03:43:55 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,19 +7,14 @@ const TARGET_URL = "https://psychic-trout-v6q7r5x7xw9g37gx-8006.app.github.dev/"
 const FALLBACK_URL = "https://win4web.dpdns.org/index2.html";
 
 app.get("/", async (req, res) => {
-  const controller = new AbortController();
-  const timeout = setTimeout(() => controller.abort(), 4000);
-
   try {
-    const response = await fetch(TARGET_URL, { method: "HEAD", signal: controller.signal });
-    clearTimeout(timeout);
-
+    const response = await fetch(TARGET_URL, { method: "HEAD", timeout: 4000 });
     if (response.ok) {
       return res.redirect(TARGET_URL);
     }
-    return res.redirect(FALLBACK_URL);
+    res.redirect(FALLBACK_URL);
   } catch (err) {
-    return res.redirect(FALLBACK_URL);
+    res.redirect(FALLBACK_URL);
   }
 });
 
